2e5520f6d558d89e134364cd6d9edb9b251dc7db
erlams <el@gmail.com>
2026-04-11 13:47:35 +0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const num1 = +prompt('eded 1 daxil edin: ')
+  const num2 = +prompt('eded 2daxil edin: ')
+
+  return (
+    <>
+      <h1>Elmaddin Ahmadzada</h1>
+      <h1>{num1} + {num2} = {num1+num2}</h1>
+    </>
+  )
+}
+
+export default App
